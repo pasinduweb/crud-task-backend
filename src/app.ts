4e5a9fbc0e7b1ef259d5dfa5dfc313d0cfab57d8
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-07 23:57:06 +0530

    Port config from env

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
     res.json({ status: 'OK' });
